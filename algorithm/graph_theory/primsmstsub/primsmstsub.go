cb49062b9ace5f9b2ce5e42064dc52ffec5c0199
Omid Houshyar <ohoushyar@gmail.com>
2016-11-22 01:45:36 -0500

    Add Prim algorithm

--- algorithm/graph_theory/primsmstsub/primsmstsub.go
+++ algorithm/graph_theory/primsmstsub/primsmstsub.go
@@ -6,36 +6,16 @@ import (
 )
 
 type Edge struct {
-    Vert Vertex
+    Src *Vertex
+    Dest *Vertex
     Len int
 }
 
 type Vertex struct {
     Name int
-    Adj map[int]bool
+    Adj []Edge
     Dist int
     Seen bool
-}
-
-// An IntHeap is a min-heap of ints.
-type EdgeHeap []Edge
-
-func (h EdgeHeap) Len() int           { return len(h) }
-func (h EdgeHeap) Less(i, j int) bool { return h[i].Len < h[j].Len }
-func (h EdgeHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *EdgeHeap) Push(x interface{}) {
-    // Push and Pop use pointer receivers because they modify the slice's length,
-    // not just its contents.
-    *h = append(*h, x.(int))
-}
-
-func (h *EdgeHeap) Pop() interface{} {
-    old := *h
-    n := len(old)
-    x := old[n-1]
-    *h = old[0 : n-1]
-    return x
 }
 
 func main() {
@@ -43,25 +23,152 @@ func main() {
     fmt.Scanf("%v", &n)
     fmt.Scanf("%v\n", &m)
 
-    graph := make(map[int]Edge)
+    graph := make(map[int]*Vertex)
     for i:=1; i<=n; i++ {
-        l := make(map[int]bool)
-        graph[i] = Edge{
-            Vertex{i, l, -1, false},
-            0,
+        graph[i] = &Vertex{
+            Name: i,
+            Adj: make([]Edge, 0),
+            Dist: -1,
+            Seen: false,
         }
     }
 
-
     for i:=0; i<m; i++ {
-        var node, adj, len int
-        fmt.Scanf("%v", &node)
-        fmt.Scanf("%v", &adj)
+        var src, dest, len int
+        fmt.Scanf("%v", &src)
+        fmt.Scanf("%v", &dest)
         fmt.Scanf("%v\n", &len)
-        fmt.Printf("n: %v a: %v l: %v\n", node, adj, len)
+        fmt.Printf("n: %v a: %v l: %v\n", src, dest, len)
+
+        e := Edge{ graph[src], graph[dest], len }
+        graph[src].Adj = append(graph[src].Adj, e)
+        // fmt.Printf("graph[%v]: %v\n", src, graph[src])
+
+        // bi-directional
+        src, dest = dest, src
+        e = Edge{ graph[src], graph[dest], len }
+        graph[src].Adj = append(graph[src].Adj, e)
+        // fmt.Printf("graph[%v]: %v\n", src, graph[src])
     }
+
+    print_g(graph)
 
     var s int
     fmt.Scanf("%v\n", &s)
     fmt.Printf("start: %v\n", s)
+
+    fmt.Println(prim_mst(graph, s))
+}
+
+func print_g(g map[int]*Vertex) {
+    for k, v:= range g {
+        fmt.Println(k)
+        for i := range(v.Adj) {
+            e := v.Adj[i]
+            fmt.Printf("dest: %v len: %v - ", e.Dest.Name, e.Len)
+        }
+        fmt.Println()
+    }
+}
+
+func prim_mst(g map[int]*Vertex, start int) int {
+    fmt.Println("--- mst ---")
+    pq := make(PriorityQueue, 0)
+    idx := 0
+    heap.Init(&pq)
+
+    mst := make(map[int]*Vertex)
+    mst[start] = g[start]
+    mst[start].Seen = true
+    fmt.Println(start)
+    for i:=0; i<len(mst[start].Adj); i++ {
+        fmt.Printf("%v ", mst[start].Adj[i].Dest.Name)
+        if !mst[start].Adj[i].Dest.Seen {
+            mst[start].Adj[i].Dest.Seen = true
+            item := &Item{
+                value: &(mst[start].Adj[i]),
+                index: idx,
+            }
+            heap.Push(&pq, item)
+            idx++
+        }
+    }
+    fmt.Println()
+
+    res := 0
+    for pq.Len() > 0 {
+        item := heap.Pop(&pq).(*Item)
+
+        edge := item.value
+        fmt.Printf("%v -> %v (%v):", edge.Src.Name, edge.Dest.Name, edge.Len)
+        res += edge.Len
+        fmt.Printf("%v\n", res)
+
+
+        ix := int(edge.Dest.Name)
+        fmt.Println(ix)
+        mst[ix] = edge.Dest
+        for i:=0; i<len(mst[ix].Adj); i++ {
+            fmt.Printf("%v ", mst[ix].Adj[i].Dest.Name)
+            if !mst[ix].Adj[i].Dest.Seen {
+                mst[ix].Adj[i].Dest.Seen = true
+                item := &Item{
+                    value: &(mst[ix].Adj[i]),
+                    index: idx,
+                }
+                heap.Push(&pq, item)
+                idx++
+            }
+        }
+        fmt.Println()
+    }
+
+    fmt.Printf("res: %v\n", res)
+    fmt.Println()
+    return res
+}
+
+// An Item is something we manage in a priority queue.
+type Item struct {
+	value    *Edge // The value of the item; arbitrary.
+	// The index is needed by update and is maintained by the heap.Interface methods.
+	index int // The index of the item in the heap.
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return pq[i].value.Len < pq[j].value.Len
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and value of an Item in the queue.
+func (pq *PriorityQueue) update(item *Item, value *Edge) {
+	item.value = value
+	heap.Fix(pq, item.index)
 }
